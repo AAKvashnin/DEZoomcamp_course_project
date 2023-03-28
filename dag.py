e3c9a3bc773881127f8c231a59da4fe42d66f0e1
AAKvashnin <alexey.kvashnin@gmail.com>
2023-03-28 22:39:18 +0300

    file to load data

--- dag.py
+++ dag.py
@@ -2,9 +2,21 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 import kaggle
+from pathlib import Path
+import boto3
+
 
 def download_dataset(ds:str, dwpath:str, filename:str,**context):
- kaggle.api.dataset_download_files(ds,dwpath,force=True,unzip=True)
+    kaggle.api.dataset_download_files(ds,dwpath,force=True,unzip=True)
+
+def write_cloud(dwpath:str, filename:str, bucket:str, **context):
+    path = Path(f"{dwpath}/{filename}")
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net'
+    )
+    s3.upload_file(path.as_posix(), bucket, path.as_posix())
 
 with DAG(dag_id='dag',
          default_args={'owner': 'airflow'},
@@ -20,4 +32,14 @@ with DAG(dag_id='dag',
                'dwpath': '/tmp',
                'filename':'onlinefraud.csv'}
        )
-   extract_data
+
+   load_datalake = PythonOperator(
+           task_id='load_datalake',
+           python_callable=write_cloud,
+           op_kwards={
+                'dwpath': '/tmp',
+                'filename':'onlinefraud.csv',
+                'bucket':'dtc-data-lake'}
+       )
+
+   extract_data >> load_datalake
