47469ad79d1ee610babb8db12ad74c4e6e128db8
AAKvashnin <alexey.kvashnin@gmail.com>
2023-04-01 21:38:49 +0300

    file to load data

---
+++ airflow/airflow/airflow/dags/dag.py
@@ -0,0 +1,52 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+import kaggle
+from pathlib import Path
+import boto3
+
+
+def download_dataset(ds:str, dwpath:str, filename:str,**context):
+    kaggle.api.dataset_download_files(ds,dwpath,force=True,unzip=True)
+
+def write_cloud(dwpath:str, filename:str, bucket:str, **context):
+    path = Path(f"{dwpath}/{filename}")
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net'
+    )
+    s3.upload_file(path.as_posix(), bucket, path.as_posix())
+
+with DAG(dag_id='dag',
+         default_args={'owner': 'airflow'},
+         schedule_interval='@daily', # Интервал запусков
+         start_date=days_ago(1) # Начальная точка запуска
+    ) as dag:
+
+   extract_data = PythonOperator(
+           task_id='extract_data',
+           python_callable=download_dataset,
+           op_kwargs={
+               'ds': 'jainilcoder/online-payment-fraud-detection',
+               'dwpath': '/tmp',
+               'filename':'onlinefraud.csv'}
+       )
+
+   load_datalake = PythonOperator(
+           task_id='load_datalake',
+           python_callable=write_cloud,
+           op_kwargs={
+                'dwpath': '/tmp',
+                'filename':'onlinefraud.csv',
+                'bucket':'dtc-data-lake'}
+       )
+
+   load_dwh=SparkSubmitOperator(
+           application='./load_dwh_pyspark.py',
+           task_id='load_dwh',
+           conn_id='spark_local'
+       )
+
+   extract_data >> load_datalake >> load_dwh
