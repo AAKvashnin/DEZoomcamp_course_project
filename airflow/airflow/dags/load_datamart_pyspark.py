3ed1475fb3a8b018d7c86c15ddec225f2921a7d4
AAKvashnin <alexey.kvashnin@gmail.com>
2023-04-06 09:32:51 +0300

    moved load data mart

--- airflow/airflow/dags/load_datamart_pyspark.py
+++ airflow/airflow/dags/load_datamart_pyspark.py
@@ -1,6 +1,9 @@
 # pyspark --packages org.postgresql:postgresql:42.6.0 --driver-memory 4G --executor-memory 4G --num-executors 2
-
 import pyspark.sql.functions as F
+from  pyspark.sql.types import IntegerType
+from  pyspark.sql.types import DecimalType
+from pyspark.sql import SparkSession
+
 
 df=spark.read.format("jdbc").option("url","jdbc:postgresql:online_fraud").\
                                          option("dbtable","dwh.online_transaction").option("user","root").\
